e357aa621131f0468067fe6a76b1500a99de70da
neoshub <neoshub@gmail.com>
2021-12-01 13:33:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-fzohdn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-fzohdn)
---
+++ src/app/app.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, VERSION } from '@angular/core';
+import { Todo } from './todomodel';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  todos: Todo[];
+
+  inputTodo: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.todos = [
+      {
+        content: 'First todo',
+        completed: false,
+      },
+      {
+        content: 'Second todo',
+        completed: false,
+      },
+    ];
+  }
+
+  toggleDone(id: number) {
+    this.todos.map((v, i) => {
+      if (i == id) v.completed = !v.completed;
+
+      return v;
+    });
+  }
+
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter((v, i) => i !== id);
+  }
+
+  addTodo() {
+    if (this.inputTodo) {
+      this.todos.push({
+        content: this.inputTodo,
+        completed: false,
+      });
+      this.inputTodo = '';
+    }
+  }
+}
